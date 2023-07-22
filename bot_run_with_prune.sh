80be77006a9d936edb19e6e5034ad939808fa6fb
Denis Donskoy <50530415+doublebon@users.noreply.github.com>
2023-07-23 02:54:40 +0300

    Update bot_run_with_prune.sh

--- bot_run_with_prune.sh
+++ bot_run_with_prune.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t telbot/bot:1 . && docker run --name telbot -itd --rm telbot/bot:1; docker image prune
+docker build -t telbot/bot:1 . && docker run --name telbot -itd --restart=always telbot/bot:1; docker image prune -f
